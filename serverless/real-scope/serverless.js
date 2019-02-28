43d959d5e6d96050b5ca83844670a4790e648de7
ymakhno <ymakhno@gmail.com>
2019-02-28 02:44:13 +0200

    Create serverless.js

---
+++ serverless/real-scope/serverless.js
@@ -0,0 +1,8 @@
+module.exports = (functionsBuilder) =>
+  functionsBuilder
+    .withNamespace('example')
+    .addWebFunction('POST', '/', async (ctx, req) => {
+      const {wix} = require('@wix/adi-waas-platformized-api/dist/proto-generated');
+      const result = await ctx.rpcClient(wix.adi.api.waas.v1.SiteService, '@wix/adi-waas-platformized-api').clear(ctx.aspects, {instanceId: 'instanceId'});
+      return result;
+    });
