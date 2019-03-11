f9ccb2e93cf1e9ae855fde7b63b6c993afcf28f6
ymakhno <iuriim@wix.com>
2019-03-11 13:28:32 +0200

    Update serverless.js

--- serverless/real-scope/serverless.js
+++ serverless/real-scope/serverless.js
@@ -7,5 +7,5 @@ module.exports = (functionsBuilder) =>
       return result;
     })
     .addWebFunction('GET', '/get', async (ctx, req) => {
-      return {version: 3, message: 'message'};
+      return {version: 4, message: 'message'};
     });
