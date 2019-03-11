598fcdb8dad0e277c76a7ff286f10f19de21167d
ymakhno <iuriim@wix.com>
2019-03-11 15:33:49 +0200

    Update serverless.js

--- serverless/real-scope/serverless.js
+++ serverless/real-scope/serverless.js
@@ -4,10 +4,10 @@ module.exports = (functionsBuilder) =>
   functionsBuilder
     .withNamespace('example')
     .addWebFunction('POST', '/', async (ctx, req) => {
-      await ctx.datastore.put('data', 'value');
+      await ctx.datastore.put('data', {val: 'value'});
       return new FullHttpResponse({status: 204, body: {}});
     })
     .addWebFunction('GET', '/get', async (ctx, req) => {
       const value = await ctx.datastore.get('data');
-      return {version: 7, message: 'message', anotherField: value};
+      return {version: 8, message: 'message', ...value};
     });
