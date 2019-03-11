8bc882577359eaf3cffc0a4434c37736c1ceaf77
ymakhno <iuriim@wix.com>
2019-03-11 15:28:55 +0200

    Update serverless.js

--- serverless/real-scope/serverless.js
+++ serverless/real-scope/serverless.js
@@ -1,11 +1,13 @@
+const {FullHttpResponse} = require('@wix/serverless-api');
+
 module.exports = (functionsBuilder) =>
   functionsBuilder
     .withNamespace('example')
     .addWebFunction('POST', '/', async (ctx, req) => {
-      const {wix} = require('@wix/adi-waas-platformized-api/dist/proto-generated');
-      const result = await ctx.rpcClient(wix.adi.api.waas.v1.SiteService, '@wix/adi-waas-platformized-api').clear(ctx.aspects, {instanceId: 'instanceId'});
-      return result;
+      await ctx.datastore.put('data', 'value');
+      return new FullHttpResponse({status: 204, body: {}});
     })
     .addWebFunction('GET', '/get', async (ctx, req) => {
-      return {version: 6, message: 'message', anotherField: 'hello'};
+      const value = await ctx.datastore.get('data');
+      return {version: 7, message: 'message', anotherField: value};
     });
