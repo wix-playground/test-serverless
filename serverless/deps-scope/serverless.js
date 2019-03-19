810f4b2efb6c9f50aa184d7f291e36523cfa50a6
Oleg Tarapata <oleht@wix.com>
2019-03-19 18:15:07 +0200

    Use moment lib in web endpoint

--- serverless/deps-scope/serverless.js
+++ serverless/deps-scope/serverless.js
@@ -1,13 +1,9 @@
 const {FullHttpResponse} = require('@wix/serverless-api');
+const moment = require('moment');
 
 module.exports = (functionsBuilder) =>
   functionsBuilder
-    .withNamespace('example')
-    .addWebFunction('POST', '/', async (ctx, req) => {
-      await ctx.datastore.put('data', {val: 'value'});
-      return new FullHttpResponse({status: 204, body: {}});
-    })
-    .addWebFunction('GET', '/get', async (ctx, req) => {
-      const value = await ctx.datastore.get('data');
-      return {version: 8, message: 'message', ...value};
+    .withNamespace('moment')
+    .addWebFunction('GET', '/now', async (ctx, req) => {
+      return {date: moment().format()};
     });
