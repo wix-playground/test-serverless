ea77c60f52b809a70d3267afb6b59efeaba7d675
ymakhno <iuriim@wix.com>
2019-04-07 00:04:17 +0300

    Update serverless.js

--- serverless/real-scope/serverless.js
+++ serverless/real-scope/serverless.js
@@ -1,9 +1,29 @@
 const {FullHttpResponse} = require('@wix/serverless-api');
+const {PaymentServicesWeb, OrderType, OrderItemCategory} = require('@wix/ambassador-payment-services-web/rpc');
+
+const description = {
+  verticalOrderId: 'verticalOrderId',
+  amount: 123.32,
+  noShipping: false,
+  currency: 'USD',
+  type: OrderType.ONE_TIME,
+  externalData: {},
+  items: [{
+    name: 'item',
+    quantity: 1,
+    description: 'description',
+    price: 123.32,
+    weightInKg: 1.0,
+    id: 'item',
+    category: OrderItemCategory.PHYSICAL
+  }]
+};
 
 module.exports = (functionsBuilder) =>
   functionsBuilder
     .withNamespace('example')
     .addWebFunction('POST', '/', async (ctx, req) => {
+      await PaymentServicesWeb().OrderService()(ctx.aspects).create({accountId: req.params.accountId, description});
       await ctx.datastore.put('data', {val: 'value'});
       return new FullHttpResponse({status: 204, body: {}});
     })
