53e96697ab768a15c25576f4138d7022563a9886
Dor <dorg@wix.com>
2019-05-26 13:48:12 +0300

    Update serverless.ts

--- serverless/typescript-scope/serverless.ts
+++ serverless/typescript-scope/serverless.ts
@@ -3,6 +3,7 @@ import {FunctionsBuilder} from '@wix/serverless-api';
 
 export = (functionsBuilder: FunctionsBuilder) =>
     functionsBuilder
+        .withNamespace('typescript-example')
         .addWebFunction('GET', '/', async (ctx, req) => {
             return {version: 1};
         });
