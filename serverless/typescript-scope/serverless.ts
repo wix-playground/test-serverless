fb1a05487a641bc61f89cd1ec011261f988685e9
Dor <dorg@wix.com>
2019-05-27 18:51:59 +0300

    Update serverless.ts

--- serverless/typescript-scope/serverless.ts
+++ serverless/typescript-scope/serverless.ts
@@ -3,6 +3,6 @@ import * as uuid from 'uuid/v4';
 const functionExport = (functionsBuilder) =>
     functionsBuilder
         .withNamespace('typescript-example')
-        .addWebFunction('GET', '/hello', () => ({data: uuid()}));
+        .addWebFunction('GET', '/hello', (ctx, req) => ({'uuid': uuid()}));
 
 module.exports = functionExport;
