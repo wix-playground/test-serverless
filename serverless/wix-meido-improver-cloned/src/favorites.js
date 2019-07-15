1e683a90903702fae0cfe6224f693ebcb3d94bfb
Dor Galam <dorg@wix.com>
2019-07-15 18:10:58 +0300

    added types: node to tsconfig.json

--- serverless/wix-meido-improver-cloned/src/favorites.js
+++ serverless/wix-meido-improver-cloned/src/favorites.js
@@ -7,18 +7,12 @@ async function getFavorites(ctx, userId) {
 }
 exports.getFavorites = getFavorites;
 async function setFavorite(ctx, userId, dishId, favorite) {
-    const favorites = {
-        ...await getFavorites(ctx, userId),
-        [dishId]: favorite,
-    };
+    const favorites = Object.assign({}, await getFavorites(ctx, userId), { [dishId]: favorite });
     await setFavorites(ctx, userId, favorites);
 }
 exports.setFavorite = setFavorite;
 async function setFavorites(ctx, userId, favorites) {
     const otherFavorites = await ctx.datastore.get(DATASTORE_KEY) || {};
-    await ctx.datastore.put('favorites', {
-        ...otherFavorites,
-        [userId]: favorites
-    });
+    await ctx.datastore.put('favorites', Object.assign({}, otherFavorites, { [userId]: favorites }));
 }
 exports.setFavorites = setFavorites;
--- serverless/wix-meido-improver-cloned/src/ratings.js
+++ serverless/wix-meido-improver-cloned/src/ratings.js
@@ -9,10 +9,7 @@ exports.getUserRatings = getUserRatings;
 async function getAvgRatings(ctx) {
     const ratings = await ctx.datastore.get(DATASTORE_KEY) || {};
     const flatRatings = [].concat(...Object.values(ratings).map(ratings => Object.entries(ratings)));
-    const allRatings = flatRatings.reduce((avg, [dishId, rating]) => ({
-        ...avg,
-        [dishId]: [...(avg[dishId] || []), rating]
-    }), {});
+    const allRatings = flatRatings.reduce((avg, [dishId, rating]) => (Object.assign({}, avg, { [dishId]: [...(avg[dishId] || []), rating] })), {});
     return Object.assign({}, ...Object.entries(allRatings)
         .map(([dishId, ratingsList]) => ({
         [dishId]: {
@@ -38,16 +35,13 @@ function avg(arr) {
 async function setRating(ctx, userId, dishId, rating) {
     const ratings = await ctx.datastore.get(DATASTORE_KEY) || {};
     const userRatings = ratings[userId] || {};
-    await ctx.datastore.put(DATASTORE_KEY, {
-        ...ratings,
-        [userId]: { ...userRatings, [dishId]: rating }
-    });
+    await ctx.datastore.put(DATASTORE_KEY, Object.assign({}, ratings, { [userId]: Object.assign({}, userRatings, { [dishId]: rating }) }));
 }
 exports.setRating = setRating;
 async function deleteRating(ctx, userId, dishId) {
     const ratings = await ctx.datastore.get(DATASTORE_KEY) || {};
-    const userRatings = { ...(ratings[userId] || {}) };
+    const userRatings = Object.assign({}, (ratings[userId] || {}));
     delete userRatings[dishId];
-    await ctx.datastore.put(DATASTORE_KEY, { ...ratings, [userId]: userRatings });
+    await ctx.datastore.put(DATASTORE_KEY, Object.assign({}, ratings, { [userId]: userRatings }));
 }
 exports.deleteRating = deleteRating;
