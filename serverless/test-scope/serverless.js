e269fcb17945ecc4c6596c690c74e242ccfde601
Mykola Borozdin <mykolab@wix.com>
2019-09-12 14:31:51 +0300

    Add datastore.list test functions #serverless test-scope

--- serverless/test-scope/serverless.js
+++ serverless/test-scope/serverless.js
@@ -8,5 +8,21 @@ module.exports = (functionsBuilder, initCtx) => {
         })
         .addWebFunction('GET', '/secret', async (ctx, req) => {
             return ctx.getConfig(req.params.secretName);
-        });
+        })
+       .addWebFunction('POST', `/generate/:prefix`, async (ctx, req) => {
+           const start = Date.now();
+           const prefix = req.params.prefix;
+           const promises = [];
+           for (let i = 0; i < 1000; i++) {
+               promises[i] = ctx.datastore.put(`${prefix}_${i}`, 'some value');
+           }
+           await Promise.all(promises);
+           return {timePassed: Date.now - start};
+       })
+      .addWebFunction('GET', `/read/:prefix`, async (ctx, req) => {
+          const start = Date.now();
+          const prefix = req.params.prefix;
+          const kvPairs = await ctx.datastore.list(prefix);
+          return {timePassed: Date.now - start};
+      });
 };
