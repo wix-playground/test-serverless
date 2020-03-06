92ba1c7ace2aa0db16734e7165c1f044839a9043
Volodymyr Polishchuk <volodymyrp@wix.com>
2020-03-06 10:22:30 +0200

    add test builder

---
+++ serverless/test-builder/serverless.js
@@ -0,0 +1,18 @@
+
+const topic = 'serverless-deployments-to-build';
+
+module.exports = (functionsBuilder) =>
+  functionsBuilder
+    .addWebFunction('GET', '/:ogr/:repo/:scope/:ref', async (ctx) => {
+      const message = {
+           deployment:{
+              ogr: req.params.org,
+              repo: req.params.repo,
+              scope: req.params.scope,
+              ref: req.params.ref
+           },
+           tryNumber: 0
+        };
+      await ctx.kafka.produceToTopic(topic ,message);
+      return {};
+    });
