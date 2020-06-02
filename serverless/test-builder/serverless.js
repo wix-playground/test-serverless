5ec2437f0c1ddaaeb465f02a8bca063c7b8a138d
Eugene Obrezkov <yevheniio@wix.com>
2020-06-02 15:49:39 +0300

    #serverless test-builder

--- serverless/test-builder/serverless.js
+++ serverless/test-builder/serverless.js
@@ -1,18 +1,34 @@
+const axios = require("axios");
 
-const topic = 'serverless-deployments-to-build';
+const DEPLOYMENTS_TO_BUILD = "serverless-deployments-to-build";
+const ACTIVE_DEPLOYMENTS_URL = "https://bo.wix.com/serverless-deployer-service/deployments";
 
-module.exports = (functionsBuilder) =>
-  functionsBuilder
-    .addWebFunction('GET', '/:org/:repo/:scope/:ref', async (ctx, req) => {
+module.exports = function (builder) {
+  return builder
+    .addWebFunction("GET", "/:org/:repo/:scope/:ref", async (context, req) => {
+      const { org, repo, scope, ref } = req.params;
       const message = {
-           deployment:{
-              org: req.params.org,
-              repo: req.params.repo,
-              scope: req.params.scope,
-              ref: req.params.ref
-           },
-           tryNumber: 0
+        deployment: { org, repo, scope, ref },
+        collaborator: "serverless-test-builder@no-mail.com",
+      };
+
+      await context.kafka.produceToTopic(DEPLOYMENTS_TO_BUILD, message);
+      return message;
+    })
+    .addWebFunction("GET", "/all", async (context) => {
+      const response = await axios.get(ACTIVE_DEPLOYMENTS_URL);
+      const deployments = response.deployments;
+      const promises = deployments.map(async (deployment) => {
+        const { org, repo, scope, ref } = deployment;
+        const message = {
+          deployment: { org, repo, scope, ref },
+          collaborator: "serverless-test-builder@no-mail.com",
         };
-      await ctx.kafka.produceToTopic(topic ,message);
-      return {};
+
+        return await context.kafka.produceToTopic(DEPLOYMENTS_TO_BUILD, message);
+      });
+
+      const result = await Promise.allSettled(promises);
+      return result;
     });
+};
