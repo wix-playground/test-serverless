ade0b431af893ed48e25592e2850f18b3c203a15
Mykola Borozdin <mykolab@wix.com>
2020-08-07 13:08:48 +0300

    #serverless cron-test-scope anotherExampleCronFunction

--- serverless/cron-test-scope/serverless.ts
+++ serverless/cron-test-scope/serverless.ts
@@ -2,24 +2,13 @@ import {FunctionsBuilder} from '@wix/serverless-api/dist';
 
 module.exports = (functionsBuilder: FunctionsBuilder) =>
   functionsBuilder
-    .addWebFunction('GET', '/someFunction', async (ctx, req) => {
-      ctx.logger.info('Got call to /someFunc');
-    })
-    .addWebFunction('GET', '/someOtherFunction', async (ctx, req) => {
-      ctx.logger.info('Got call to /someOtherFunc');
-    })
-    .addCronJob(
-      'GET',
-      '/someFunction',
-      '0 * * ? * *'
-    )
-    .addCronJob(
-      'GET',
-      '/someOtherFunction',
-      '0 * * ? * *'
-    )
     .addCronFunction(
       'exampleCronFunction',
       '0 * * ? * *',
       async (ctx) => ctx.logger.info('exampleCronFunction called')
+    )
+    .addCronFunction(
+      'anotherExampleCronFunction',
+      '0 * * ? * *',
+      async (ctx) => ctx.logger.info('anotherExampleCronFunction called')
     );
