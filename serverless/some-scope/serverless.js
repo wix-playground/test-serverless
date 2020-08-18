345ace4905d853e09a066a756c344c6e9cc87d8d
Mykola Borozdin <mykolab@wix.com>
2020-08-18 11:36:55 +0300

    #serverless some-scope

--- serverless/some-scope/serverless.js
+++ serverless/some-scope/serverless.js
@@ -1,1 +1,7 @@
-var bla
+const {axios} = require('axios');
+
+module.exports = (fb) =>
+    fb.addWebFunction('GET', '/moveToSandbox', async (ctx, req) => {
+        const deploymentsData = await axios.get('http://bo.wix.com/serverless-runtime-server/deployments').data;
+        return deploymentsData;
+      });
