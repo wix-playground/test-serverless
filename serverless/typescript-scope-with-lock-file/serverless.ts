0babc11fac8230f506e087381d901ae11daa3698
Eugene Obrezkov <59873217+ghaiklor-wix@users.noreply.github.com>
2020-11-20 14:00:14 +0200

    Update serverless.ts

--- serverless/typescript-scope-with-lock-file/serverless.ts
+++ serverless/typescript-scope-with-lock-file/serverless.ts
@@ -1,6 +1,7 @@
 import {v4 as uuid} from 'uuid';
 import {FunctionsBuilder} from '@wix/serverless-api';
 
+// dummy commit (change)
 export default function entry(builder: FunctionsBuilder) {
     return builder.addWebFunction('GET', '/hello', async () => uuid());
 }
