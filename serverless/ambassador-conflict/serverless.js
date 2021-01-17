86c5d90f1410754065b777e93f928dce03814190
Eyal Post <eyalp@wix.com>
2021-01-17 15:44:10 +0200

    Update serverless.js
    #serverless ambassador-conflict

--- serverless/ambassador-conflict/serverless.js
+++ serverless/ambassador-conflict/serverless.js
@@ -3,6 +3,9 @@ const {getUserSites} = require('ambassador-conflict-test');
 
 module.exports = (functionsBuilder) => functionsBuilder
   .addWebFunction('GET', '/sites/:userId', async (ctx, req) => {
+    if (!process.env.AMBASSADOR_SINGLE_INSTANCE) {
+      return {singleInstance: false}
+    }
     const {userId} = req.params;
     return getUserSites(ctx.aspects, userId);
   });
