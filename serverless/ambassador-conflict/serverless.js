cc754b896082743c300999de1cf7a227a4669ef1
Eyal Post <eyalp@wix.com>
2021-01-17 15:38:58 +0200

    Dummy commit
    #serverless ambassador-conflict

--- serverless/ambassador-conflict/serverless.js
+++ serverless/ambassador-conflict/serverless.js
@@ -1,5 +1,6 @@
 const {getUserSites} = require('ambassador-conflict-test');
 
+
 module.exports = (functionsBuilder) => functionsBuilder
   .addWebFunction('GET', '/sites/:userId', async (ctx, req) => {
     const {userId} = req.params;
