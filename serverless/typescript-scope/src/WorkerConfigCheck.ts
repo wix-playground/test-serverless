e4d690a0f37764cf26061137a3d4a1d9ee835acc
Mykola Borozdin <mykolab@wix.com>
2021-02-11 14:27:44 +0200

    #serverless typescript-scope

--- serverless/typescript-scope/src/WorkerConfigCheck.ts
+++ serverless/typescript-scope/src/WorkerConfigCheck.ts
@@ -18,10 +18,10 @@ async function checkArtifactIds(artifactIds: string[], ctx: FunctionContext) {
 async function checkWorkerConfig(artifactId: string, ctx: FunctionContext) {
   const page = (await axios(`https://fryingpan.wixpress.com/services/${artifactId}/edit`)).data;
   ctx.logger.info(`Got response: ${page}`);
-  const dom = new JSDOM(page/* , null, {
+  const dom = new JSDOM(page, {
     features: {
       QuerySelector: true
     }
-  } */);
+  });
   ctx.logger.info(`Got dom: ${JSON.stringify(dom.window.document.querySelector)}`);
 }
