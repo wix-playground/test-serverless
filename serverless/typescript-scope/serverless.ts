08e3603f7ebb884dbf2273596163bb7052f2857c
Mykola Borozdin <mykolab@wix.com>
2021-02-12 11:53:21 +0200

    #serverless typescript-scope

--- serverless/typescript-scope/serverless.ts
+++ serverless/typescript-scope/serverless.ts
@@ -6,7 +6,7 @@ const functionExport = (functionsBuilder: FunctionsBuilder) =>
     functionsBuilder
         .addWebFunction('GET', '/hello', async () => uuid())
         .addWebFunction('GET', '/check', async (ctx, req) => {
-          return await checkWorkerConfigs(ctx, req.cookies);
+          return await checkWorkerConfigs(ctx, req.params.authToken);
         });
 
 module.exports = functionExport;
--- serverless/typescript-scope/src/WorkerConfigCheck.ts
+++ serverless/typescript-scope/src/WorkerConfigCheck.ts
@@ -1,25 +1,23 @@
 import type { FunctionContext } from '@wix/serverless-api';
 import axios from 'axios';
 
-export async function checkWorkerConfigs(ctx: FunctionContext, cookies: Record<string, string>) {
+export async function checkWorkerConfigs(ctx: FunctionContext, authToken: string) {
   const response = await axios.get('http://api.42.wixprod.net/serverless-deployer-service/v2/artifacts');
   //const metadata = response.data.metadata;
   const artifactIds = response.data.artifactIds;
-  return await checkArtifactIds(artifactIds, ctx, cookies);
+  return await checkArtifactIds(artifactIds, ctx, authToken);
 }
 
-async function checkArtifactIds(artifactIds: string[], ctx: FunctionContext, cookies: Record<string, string>) {
+async function checkArtifactIds(artifactIds: string[], ctx: FunctionContext, authToken: string) {
   return await Promise.all(artifactIds.map(async (id) =>
-    await checkWorkerConfig(id, ctx, cookies)
+    await checkWorkerConfig(id, ctx, authToken)
   ));
 }
 
-async function checkWorkerConfig(artifactId: string, ctx: FunctionContext, cookies: Record<string, string>): Promise<boolean> {
-  const cookieString = Object.keys(cookies).map((key) => `${key}=${cookies[key]}`).join('; ');
-  ctx.logger.info(`Sending cookies: ${cookieString}`);
+async function checkWorkerConfig(artifactId: string, ctx: FunctionContext, authToken: string): Promise<boolean> {
   const page = (await axios.get(`https://fryingpan.wixpress.com/api/v2/services/${artifactId}/configs`, {
     headers: {
-      'Cookie': cookieString
+      'Authorization': `Bearer ${authToken}`
     }
   })).data;
   ctx.logger.info(`Got response: ${page}`);
