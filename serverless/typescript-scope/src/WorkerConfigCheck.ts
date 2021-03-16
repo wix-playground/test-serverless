a9e98dad54514ea2271234c6c7378db61f6b2e2c
Mykola Borozdin <mykolab@wix.com>
2021-03-16 20:29:57 +0200

    #serverless typescript-scope

--- serverless/typescript-scope/src/WorkerConfigCheck.ts
+++ serverless/typescript-scope/src/WorkerConfigCheck.ts
@@ -20,12 +20,16 @@ export async function checkWorkerConfigs(ctx: FunctionContext, authToken: string
   ctx.logger.info(`Gott metadata: ${JSON.stringify(response.data.metadata)}`);
   ctx.logger.info(`Gotttt metadata: ${JSON.stringify(metadata)}`);
   var offset;
-  for (offset = 0; offset < metadata.total; offset += metadata.count) {
-    const artifactIds = (await axios.get(`http://api.42.wixprod.net/serverless-deployer-service/v2/artifacts?offset=${offset}`)).data.artifactIds;
-    ctx.logger.info(`For offset ${offset}: got artifactIds ${JSON.stringify(artifactIds)}`);
-    promisesArray.push(checkArtifactIds(artifactIds, ctx, authToken));
+  try {
+    for (offset = 0; offset < metadata.total; offset += metadata.count) {
+      const artifactIds = (await axios.get(`http://api.42.wixprod.net/serverless-deployer-service/v2/artifacts?offset=${offset}`)).data.artifactIds;
+      ctx.logger.info(`For offset ${offset}: got artifactIds ${JSON.stringify(artifactIds)}`);
+      promisesArray.push(checkArtifactIds(artifactIds, ctx, authToken));
+    }
+    return await Promise.all(promisesArray);
+  } catch (err) {
+    ctx.logger.error(`Failed with ${JSON.stringify(err)}`)
   }
-  return await Promise.all(promisesArray);
 }
 
 async function checkArtifactIds(artifactIds: string[], ctx: FunctionContext, authToken: string) {
