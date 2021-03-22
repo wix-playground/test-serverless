0fb5c7389b4aa702a00c5f8030e650e42795ecd9
Mykola Borozdin <mykolab@wix.com>
2021-03-22 22:46:10 +0200

    Another deploy breaker #serverless typescript-scope

--- serverless/typescript-scope/serverless.ts
+++ serverless/typescript-scope/serverless.ts
@@ -5,12 +5,10 @@ import { checkWorkerConfigs } from './src/WorkerConfigCheck';
 
 const functionExport = (functionsBuilder: FunctionsBuilder) =>
     functionsBuilder
-      .whenStarted(() => {
-        throw 'This should break deploy';
-      })
       .addWebFunction('GET', '/hello', async () => 'Some: v1 + ' + uuid())
       .addWebFunction('GET', '/check', {timeoutMillis: 900000}, async (ctx, req) => {
         return await checkWorkerConfigs(ctx, req.query.authToken, req.query.offset);
       });
 
+if (Math.random() > 0) throw 'This definitely should break deploy';
 module.exports = functionExport;
