13a062960fb3812dad80393d3dc7a564cebfc771
Mykola Borozdin <mykolab@wix.com>
2024-04-06 15:25:06 +0300

    #s8s typescript-scope add

--- serverless/typescript-scope/serverless.ts
+++ serverless/typescript-scope/serverless.ts
@@ -43,7 +43,9 @@ module.exports = (functionsBuilder: FunctionsBuilder) =>
             'Content-Type': 'application/json'
           } 
         });
-        const fpServices: Array<string> = fpServicesResponse.data.map((serviceJson) => serviceJson.id.match(/\.([a-zA-Z0-9/-]+$)/)[1]);
+        const fpServices: Array<string> = fpServicesResponse.data.map((serviceJson) => { const match  = serviceJson.id.match(/\.([a-zA-Z0-9/-]+$)/);
+      ctx.logger.info(`Trying to match ${serviceJson.id} and got ${JSON.stringify(result)}`);
+    return match[1]; });
         const result = fpServices.filter((value) => applications.applicationIds.includes(value));
         ctx.logger.info(`Got conflicting applicationIds: ${JSON.stringify(result)}`)
         return result;
